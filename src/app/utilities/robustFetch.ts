6caaac9c32e4db83be98a79cc098216ffcbce2c2
Adam Karacsony <adam.karacsony@gmail.com>
2024-03-20 17:30:52 +0100

    robustFetch

---
+++ src/app/utilities/robustFetch.ts
@@ -0,0 +1,60 @@
+const futileCodes = ['403', '404', '507', '508', '511'];
+
+type Summary = {
+  error: string,
+  responseCode: number,
+  responseBody: string
+}
+
+interface RobustFetchParams {
+  url: string;
+  abortSignal?: AbortSignal;
+  retries?: number;
+  timer?: number;
+  verbose?: boolean;
+  logger?: (summary: Summary) => void;
+}
+
+export default function robustFetch<T>({ url, abortSignal, retries = 3, timer = 300, verbose, logger }: RobustFetchParams): Promise<T|Error> {
+  // const isVerbose = typeof verbose !== 'undefined' ? verbose : import.meta.env.MODE === 'dev' || import.meta.env.MODE === 'testing'; // Vite SPA
+  const isVerbose = typeof verbose !== 'undefined' ? verbose : process.env.NODE_ENV === 'development'; // Next.js
+
+  let currentResponse: Response;
+
+  return fetch(url, {signal: abortSignal})
+    .then(response => {
+      currentResponse = response;
+
+      if(!response.ok) {
+        throw new Error(`${response.status}`);
+      }
+
+      return response.json().catch(() => {
+        throw new Error('json');
+      });
+    }).catch(async error => {
+
+      // Aborted. Exit without logging
+      if(abortSignal?.aborted) {
+        throw error; // exit, no retries.
+      }
+
+      // Ended with failure, log if needed
+      if(retries === 0 || futileCodes.indexOf(error.message) >= 0 || error.message === 'json') {
+        const summary: Summary = {
+          error: error.message,
+          responseCode: currentResponse.status,
+          responseBody: await currentResponse.text()
+        }
+        isVerbose && console.log('Fetch failed', summary);
+        logger?.(summary);
+
+        throw error; // exit, no retries.
+      }
+
+      // Failure, but can retry
+      isVerbose && console.log(`Retrying fetch (${url})... Attempts left: ${retries}`, error.message);
+      return new Promise(resolve => setTimeout(resolve, timer))
+        .then(() => robustFetch({url, abortSignal, retries: retries - 1, timer: timer * 2, verbose, logger}));
+    });
+}
